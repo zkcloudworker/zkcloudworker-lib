66af06be040b4245c12423ed2c16cead61651e50
DFST <mk@dfst.io>
2024-03-14 20:54:27 +0400

    aws

--- dist/lib/ts/src/config.js
+++ dist/lib/ts/src/config.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const config = {
     MINAFEE: "200000000",
     ZKCLOUDWORKER_AUTH: "M6t4jtbBAFFXhLERHQWyEB9JA9xi4cWqmYduaCXtbrFjb7yaY7TyaXDunKDJNiUTBEcyUomNXJgC",
-    ZKCLOUDWORKER_API: "https://n1zjzclr99.execute-api.eu-west-1.amazonaws.com/dev/zkcloudworker",
+    ZKCLOUDWORKER_API: " https://cuq99yahhi.execute-api.eu-west-1.amazonaws.com/dev/zkcloudworker",
 };
 exports.default = config;
--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ const config = {
   ZKCLOUDWORKER_AUTH:
     "M6t4jtbBAFFXhLERHQWyEB9JA9xi4cWqmYduaCXtbrFjb7yaY7TyaXDunKDJNiUTBEcyUomNXJgC",
   ZKCLOUDWORKER_API:
-    "https://n1zjzclr99.execute-api.eu-west-1.amazonaws.com/dev/zkcloudworker",
+    " https://cuq99yahhi.execute-api.eu-west-1.amazonaws.com/dev/zkcloudworker",
 };
 
 export default config;
