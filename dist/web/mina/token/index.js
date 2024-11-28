a37f60b4f1e0bbaa468a600f103031549eacab1c
DFST <mk@dfst.io>
2024-11-28 10:25:04 +0300

    minatokens lib

--- dist/web/mina/token/index.js
+++ dist/web/mina/token/index.js
@@ -1,9 +1,2 @@
 export * from "./api.js";
-export * from "./bid.js";
-export * from "./build.js";
-export * from "./fee.js";
-export * from "./offer.js";
-export * from "./token.js";
-export * from "./vk.js";
-export * from "./whitelist.js";
 //# sourceMappingURL=index.js.map
